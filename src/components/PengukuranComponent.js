b55e73fe3a37dcbc2563f90f1bb25a63c4c60658
sepriandani <sepriandanni@gmail.com>
2022-10-03 14:55:01 +0700

    update

--- src/components/PengukuranComponent.js
+++ src/components/PengukuranComponent.js
@@ -18,7 +18,6 @@ export default function PengukuranComponent(){
     });
 
     return(
-        <>
             <div className=" px-5 lg:px-40 my-28 md:my-40 flex gap-5 justify-between text-center flex-col md:flex-row">
                 <div className="p-10 lg:w-60 bg-gray-50 rounded-md shadow-md hover:shadow-lg cursor-pointer flex-1 text-center">
                     <p className="font-semibold text-gray-500 mb-2">Tegangan</p>
@@ -34,9 +33,9 @@ export default function PengukuranComponent(){
                 </div>
                 <div className="p-10 lg:w-60 bg-gray-50 rounded-md shadow-md hover:shadow-lg cursor-pointer flex-1 text-center">
                 <p className="font-semibold text-gray-500 mb-2">Irradiant</p>
-                    <p className="text-4xl font-semibold text-gray-700">{ans.Irradiant} i</p>
+                    <p className="text-4xl font-semibold text-gray-700">{ans.Irradiant} W/m<sup>2</sup></p>
                 </div>
             </div>
-        </>
+
     )
 }
